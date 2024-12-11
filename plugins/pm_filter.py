6bead04a01e9ae6d43a5d014e6be992a8bd89744
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2024-12-11 17:38:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1712,7 +1712,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await msg.message.delete()"""
 
 async def auto_filter(client, msg, spoll=False):
-    if not msg.chat.type=="private" or not msg.reply_to_message.chat=="private":
+    if not msg.chat.type=="private":
         reqstr1 = msg.from_user.id if msg.from_user else 0
         reqstr = await client.get_users(reqstr1)
         if not spoll:
