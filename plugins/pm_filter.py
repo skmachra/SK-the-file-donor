1d672bd9b33d669734ebc49afdbb32b941500876
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2024-12-11 16:44:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1712,7 +1712,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await msg.message.delete()"""
 
 async def auto_filter(client, msg, spoll=False):
-    if not msg.message.chat:
+    if not msg.from_chat:
         reqstr1 = msg.from_user.id if msg.from_user else 0
         reqstr = await client.get_users(reqstr1)
         if not spoll:
