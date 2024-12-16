fc675780f3571701cb558574dd089e27ea6eb446
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2024-12-16 16:31:04 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -23,7 +23,6 @@ async def inline_users(query: InlineQuery):
 @Client.on_inline_query()
 async def answer(bot, query):
     """Show search results for given inline query"""
-    print(query)
     chat_id = await active_connection(str(query.from_user.id))
     
     if not await inline_users(query):
