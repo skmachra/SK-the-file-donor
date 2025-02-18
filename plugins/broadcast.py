3ac4ae64a556acbd46914b5097f70c8c6f43043e
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2025-02-18 23:09:42 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -11,6 +11,10 @@ import asyncio
 # https://t.me/GetTGLink/4178
 async def verupikkals(bot, message):
     users = await db.get_all_users()
+    # users = [
+    # {"id": 7378854236},
+    # {"id": 6151164206},
+    # {"id": 1327019706},]
     b_msg = message.reply_to_message
     sts = await message.reply_text(
         text='Broadcasting your messages...'
@@ -37,12 +41,10 @@ async def verupikkals(bot, message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            # await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
-            await sts.edit(f"Broadcast Completed:\nCompleted in 2 days, 2:06:43 seconds.\n\nTotal Users 75428\nCompleted: 75428 / 75428\nSuccess: 60514\nBlocked: 11376\nDeleted: 3538")
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+            # await sts.edit(f"Broadcast Completed:\nCompleted in 2 days, 2:06:43 seconds.\n\nTotal Users 75428\nCompleted: 75428 / 75428\nSuccess: 60514\nBlocked: 11376\nDeleted: 3538")
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
-    # await sts.edit(f"Broadcast Completed:\nCompleted in 2 days, 2:06:43 seconds.\n\nTotal Users 75428\nCompleted: 75428 / 75428\nSuccess: 60514\nBlocked: 11376\nDeleted: 3538")
-    #d
 
 @Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
 async def grp_brodcst(bot, message):
