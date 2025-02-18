6b8b2f1bf731462fae4e87b299ee051f1a113957
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2025-02-18 22:58:06 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -38,10 +38,10 @@ async def verupikkals(bot, message):
         await asyncio.sleep(2)
         if not done % 20:
             # await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
-            await sts.edit(f"Broadcast Completed:\nCompleted in 2 days, 2:06:43 seconds.\n\nTotal Users 75428nCompleted: 75428 / 75428\nSuccess: 60514\nBlocked: 11376\nDeleted: 3538")
+            await sts.edit(f"Broadcast Completed:\nCompleted in 2 days, 2:06:43 seconds.\n\nTotal Users 75428\nCompleted: 75428 / 75428\nSuccess: 60514\nBlocked: 11376\nDeleted: 3538")
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
-    # await sts.edit(f"Broadcast Completed:\nCompleted in 2 days, 2:06:43 seconds.\n\nTotal Users 75428nCompleted: 75428 / 75428\nSuccess: 60514\nBlocked: 11376\nDeleted: 3538")
+    # await sts.edit(f"Broadcast Completed:\nCompleted in 2 days, 2:06:43 seconds.\n\nTotal Users 75428\nCompleted: 75428 / 75428\nSuccess: 60514\nBlocked: 11376\nDeleted: 3538")
     #d
 
 @Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
