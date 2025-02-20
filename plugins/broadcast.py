3e3d1c108c38b8d03af49e852a6d02d46f3f079c
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2025-02-20 22:38:30 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -45,7 +45,7 @@ async def verupikkals(bot, message):
                 failed += 1
         done += 1
         await asyncio.sleep(2)
-        if not done % 1:
+        if not done % 20:
             await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
             # await sts.edit(f"Broadcast Completed:\nCompleted in 2 days, 2:06:43 seconds.\n\nTotal Users 75428\nCompleted: 75428 / 75428\nSuccess: 60514\nBlocked: 11376\nDeleted: 3538")
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
