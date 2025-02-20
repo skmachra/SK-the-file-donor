5b015cace9124f776282d35fbc0ca29532d37dec
Sunil Kumar <147473716+skmachra@users.noreply.github.com>
2025-02-20 19:56:26 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -10,11 +10,12 @@ import asyncio
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 # https://t.me/GetTGLink/4178
 async def verupikkals(bot, message):
-    # users = await db.get_all_users()
-    users = [
-    {"id": 7378854236},
-    {"id": 6151164206},
-    {"id": 1327019706},]
+    users = await db.get_all_users()
+    print(users)
+    # users = {
+    # {"id": 7378854236},
+    # {"id": 6151164206},
+    # {"id": 1327019706},}
     b_msg = message.reply_to_message
     sts = await message.reply_text(
         text='Broadcasting your messages...'
